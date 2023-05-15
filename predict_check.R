e5d1ec29fc85a36772eb96b4c7e42f4b3fbffb73
Yoonji <yoonji.noh90@gmail.com>
2023-05-15 20:22:23 +0900

    modeling

--- predict_check.R
+++ predict_check.R
@@ -15,128 +15,49 @@ ipak(pkg)
 ##########################################################################################################################################################
 
 # data load
-predict <- read_excel("/Users/yj.noh/Desktop/predict_data.xlsx", sheet = 1) 
-answer <- read_excel("/Users/yj.noh/Desktop/answer_data.xlsx")
-
-predict$날짜 <- as.Date(predict$날짜)
-answer$reg_date <- as.Date(answer$reg_date)
-
-min(predict$날짜)
-max(predict$날짜)
-min(answer$reg_date)
-max(answer$reg_date)
-
-answer <- answer  %>% filter(pick_rgn1_nm == '서울특별시' & hour_reg %in% c(9,10,11,12,13,14,15,16,17,18,19,20,21,22,23))
-
-predict <- predict  %>% filter(시간대 %in% c(9,10,11,12,13,14,15,16,17,18,19,20,21,22,23))
-
-dim(predict)
-dim(answer)
-
-library(Metrics)
-
-predict <- left_join(predict, answer[c("reg_date", "hour_reg", "pick_rgn2_nm", "라이더수")] , by = c("날짜" = "reg_date", "시간대" = "hour_reg", "지역구" = "pick_rgn2_nm"))
+predict <- read.csv("prediction_results.csv", fileEncoding = "cp949")
 
 head(predict)
+predict$datetime <- as.POSIXct(predict$datetime)
+min(predict$datetime)
+max(predict$datetime)
 
-predict <- predict  %>% 
-mutate(error = abs(예측값 - 라이더수))
+predict <- predict  %>% mutate(reg_date = as.Date(predict$datetime))
 
-colSums(is.na(predict))
-
-write.csv(predict, "predict_data.csv", row.names = FALSE, fileEncoding = "cp949")
-
-mae(predict$예측값, predict$라이더수) # 35.98
-
-# 일자별
-result_day <- aggregate(abs(predict$예측값 - predict$라이더수), 
-                    by = list(predict$날짜), 
-                    FUN = mean)
-
-result_day
-
-# 시간대별 
-result_hour <- aggregate(abs(predict$예측값 - predict$라이더수), 
-                    by = list(predict$시간대), 
-                    FUN = mean)
-result_hour
-
-# 지역별
-result_rgn2 <- aggregate(abs(predict$예측값 - predict$라이더수), 
-                    by = list(predict$지역구), 
-                    FUN = mean)
-result_rgn2
-
-# 지역별, 시간대별 
-result_total <- aggregate(abs(predict$예측값 - predict$라이더수), 
-                    by = list(predict$지역구, predict$시간대), 
-                    FUN = mean)
-result_total
-
-write.csv(result_total, "result_total.csv", row.names = FALSE, fileEncoding = "cp949")
-
-
-
-
-
-
-
-# dataset 
-# 0. install packages 
-options(scipen=10)
-
-ipak <-function(pkg){
-  new.pkg<-pkg[!(pkg %in% installed.packages()[,"Package"])]
-  if(length(new.pkg))
-    install.packages(new.pkg,dependencies=TRUE)
-  sapply(pkg,require,character.only=TRUE)
-}
-
-pkg <- c("readr", "dplyr", "tidytext", "tidyverse", "lubridate", "reshape2", "psych", "gtsummary", "readxl", "MASS") # nolint
-ipak(pkg)
-
-##########################################################################################################################################################
-
-# data load
-gn <- read_excel("/Users/yj.noh/Desktop/check_data.xlsx", sheet = 1) 
-sc <- read_excel("/Users/yj.noh/Desktop/check_data.xlsx", sheet = 2)
-
-data <- rbind(gn,sc)
-dim(data)
-head(data)
-
-data$datetime <- as.Date(data$datetime)
-data$hour <- as.factor(hour(data$hour))
-
-# total
 library(Metrics)
-mae(data$actual, data$forecast_arima) #43.87
-mae(data$actual, data$forecast_lm) #32.61
 
 # 일자별
-result_day <- aggregate(cbind(abs(data$forecast_arima - data$actual), 
-                               abs(data$forecast_lm - data$actual)),
-                        by = list(data$datetime), 
+result_day <- aggregate(cbind(abs(predict$y_pred_test_LinearRegression - predict$y_test), 
+                               abs(predict$y_pred_test_LGBMRegressor - predict$y_test)),
+                        by = list(predict$reg_date), 
                         FUN = mean)
 result_day
 
 # 시간대별 
-result_hour <- aggregate(cbind(abs(data$forecast_arima - data$actual), 
-                               abs(data$forecast_lm - data$actual)),
-                        by = list(data$hour), 
+result_hour <- aggregate(cbind(abs(predict$y_pred_test_LinearRegression - predict$y_test), 
+                               abs(predict$y_pred_test_LGBMRegressor - predict$y_test)),
+                        by = list(predict$hour_reg), 
                         FUN = mean)
 result_hour
-table(data$hour)
+
 # 지역별
-result_rgn2 <- aggregate(cbind(abs(data$forecast_arima - data$actual), 
-                               abs(data$forecast_lm - data$actual)),
-                        by = list(data$pick_rgn2_nm),
+result_rgn2 <- aggregate(cbind(abs(predict$y_pred_test_LinearRegression - predict$y_test), 
+                               abs(predict$y_pred_test_LGBMRegressor - predict$y_test)),
+                        by = list(predict$pick_rgn2_nm),
                     FUN = mean)
 result_rgn2
 
 # 지역별, 시간대별 
-result_total <- aggregate(cbind(abs(data$forecast_arima - data$actual), 
-                               abs(data$forecast_lm - data$actual)),
-                    by = list(data$pick_rgn2_nm, data$hour), 
+result_total <- aggregate(cbind(abs(predict$y_pred_test_LinearRegression - predict$y_test), 
+                               abs(predict$y_pred_test_LGBMRegressor - predict$y_test)),
+                    by = list(predict$pick_rgn2_nm, predict$hour_reg), 
                     FUN = mean)
 result_total
+
+
+# 기상
+result_rain <- aggregate(cbind(abs(predict$y_pred_test_LinearRegression - predict$y_test), 
+                               abs(predict$y_pred_test_LGBMRegressor - predict$y_test)),
+                    by = list(predict$is_rain), 
+                    FUN = mean)
+result_rain
---
+++ predict_dataset.R
@@ -0,0 +1,130 @@
+# dataset 
+# 0. install packages 
+options(scipen=10)
+
+ipak <-function(pkg){
+  new.pkg<-pkg[!(pkg %in% installed.packages()[,"Package"])]
+  if(length(new.pkg))
+    install.packages(new.pkg,dependencies=TRUE)
+  sapply(pkg,require,character.only=TRUE)
+}
+
+pkg <- c("readr", "dplyr", "tidytext", "tidyverse", "lubridate", "reshape2", "psych", "gtsummary", "readxl", "MASS") # nolint
+ipak(pkg)
+
+##########################################################################################################################################################
+# data load
+data <- read_excel("/Users/yj.noh/Desktop/predict_data.xlsx") 
+
+data <- data %>%
+  dplyr::rename(rider_cnt = 라이더수,
+                order_cnt = 주문수)
+# seoul
+min(data$reg_date) #2023-04-18
+max(data$reg_date) #2023-05-15
+
+data$reg_date <- as.Date(data$reg_date)
+
+
+head(data)
+
+data <- data %>% 
+  filter(pick_rgn1_nm == '서울특별시' & reg_date < max(data$reg_date))
+
+dim(data) # 11,675
+
+table(data$pick_rgn2_nm)
+table(data$hour_reg) 
+
+# 9am ~ 23pm 
+data <- data  %>% 
+filter(hour_reg %in% c(9,10,11,12,13,14,15,16,17,18,19,20,21,22,23))
+
+dim(data) #10125
+table(data$hour_reg) # 결측치 없음. 
+
+# 변수 생성
+# is_rain
+data <- data  %>% 
+mutate(is_rain = ifelse(reg_date == '2023-05-11', 1, 0))
+table(data$is_rain)
+
+# month, week
+data <- data  %>% 
+  mutate(month = month(reg_date),
+         week = ceiling(day(reg_date) /7),
+         day_of_reg = substr(weekdays(as.Date(reg_date)),1,3))
+
+# is_holiday
+holiday_list = ymd(c("2022-01-01", "2022-01-31", "2022-02-01", "2022-03-01", "2022-03-09",  "2022-05-05", "2022-05-08", "2022-06-01", "2022-06-06", "2022-08-15", "2022-09-09", "2022-09-10", "2022-09-11", "2022-09-12", 
+"2022-10-03",  "2022-10-09", "2022-10-10", "2022-12-25", "2023-01-01", "2023-01-21","2023-01-22", "2023-01-23", "2023-01-24", "2023-03-01", "2023-05-01", "2023-05-05","2023-05-27", "2023-05-29", "2023-06-06", "2023-08-15", "2023-09-28", "2023-09-29",
+"2023-09-30", "2023-10-03", "2023-10-09", "2023-12-25"))
+
+
+data <- data %>% 
+  mutate(is_holiday = ifelse((reg_date %in% holiday_list) | (day_of_reg %in% c("SAT", "SUN")),1,0))
+
+colSums(is.na(data))
+
+
+#w-1,2,3,4 동일 요일 동시간대 주문수/라이더수
+library(zoo)
+
+data <- data %>%
+  arrange(reg_date,pick_rgn2_nm) %>% 
+  group_by(pick_rgn2_nm, day_of_reg, hour_reg) %>% 
+  mutate(rider_cnt_w_2 = lag(rider_cnt, n=1),
+         rider_cnt_w_3 = lag(rider_cnt, n=2),
+         rider_cnt_w_4 = lag(rider_cnt, n=3),
+         order_cnt_w_2 = lag(order_cnt, n=1),
+         order_cnt_w_3 = lag(order_cnt, n=2),
+         order_cnt_w_4 = lag(order_cnt, n=3))
+
+
+str(data)
+
+# 정리
+data <- data  %>% mutate(reg_date = reg_date + 7,
+                          rider_cnt_w_1 = rider_cnt,
+                          order_cnt_w_1 = order_cnt)
+
+
+data<- data  %>% filter(reg_date >='2023-05-16')
+dim(data) #2250
+
+min(data$reg_date)
+min(data$datetime)
+
+colSums(is.na(data))
+data<- subset(data, select = -c(pick_rgn1_nm))
+
+
+train_data <- read.csv("combined_data.csv", fileEncoding = "cp949")
+train_data <- subset(train_data, select = -c(rider_cnt, order_cnt, temp_c, rain_c, snow_c, q1, q3, IQR1.5, outlier,datetime))
+
+str(train_data)
+
+test_data <- subset(data, select = -c(rider_cnt, order_cnt))
+test_data <- test_data  %>% mutate(rider_cnt_2 = NA)
+test_data <- test_data  %>% dplyr::select("pick_rgn2_nm","hour_reg", "reg_date", "day_of_reg", "is_rain", "month", "week", "is_holiday", "rider_cnt_2","rider_cnt_w_1","rider_cnt_w_2","rider_cnt_w_3","rider_cnt_w_4", "order_cnt_w_1", "order_cnt_w_2",  "order_cnt_w_3",  "order_cnt_w_4")
+
+
+test_data$reg_date <- as.Date(test_data$reg_date)
+train_data$reg_date <- as.Date(train_data$reg_dat)
+
+train_data$datetime <- as.POSIXct(train_data$datetime)
+
+min(test_data$reg_date)
+max(test_data$reg_date)
+
+min(train_data$reg_date)
+max(train_data$reg_date)
+
+
+predict_data <- rbind(train_data,test_data)
+
+
+write.csv(predict_data, "predict_data.csv", row.names = FALSE, fileEncoding = "cp949")
+
+
+
