277a06d1dc520f617d56e293ce4b23cce97f2a2f
Yoonji <yoonji.noh90@gmail.com>
2023-06-16 15:56:50 +0900

    ..

--- delivery_time_predict/lm_model.R
+++ delivery_time_predict/lm_model.R
@@ -16,6 +16,30 @@ ipak(pkg)
 # data load
 data <- read_excel("/Users/yj.noh/Desktop/infra.xlsx") 
 head(data)
+str(data)
+
+data$day <- as.factor(data$day)
+data$weather <- as.factor(data$weather)
 
 # 배차소요시간
-model1 <- lm(배차소요시간 ~)
+model1 <- lm(배차소요시간 ~ ord_cnt + weather + rider_cnt + day, data = data)
+summary(model1)
+
+model2 <- lm(배차소요시간 ~ ord_cnt + rider_cnt + day, data = data)
+summary(model2)
+coefficients(model2)
+
+y_pred <- predict(model1, data = data)
+
+library(Metrics)
+mae(data$배차소요시간, y_pred) #0.54 
+rmse(data$배차소요시간, y_pred) #0.67
+mape(data$배차소요시간, y_pred) #
+
+# 배달시간
+model3 <- lm(배달시간 ~ ord_cnt + weather + rider_cnt + day, data = data)
+summary(model3)
+
+model4 <- lm (배달시간 ~ ord_cnt + rider_cnt + day, data = data)
+summary(model4)
+coefficients(model4)
