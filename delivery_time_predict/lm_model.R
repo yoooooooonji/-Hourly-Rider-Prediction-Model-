dfb967090f2267a702e777cb297fd693610841d6
Yoonji <yoonji.noh90@gmail.com>
2023-06-16 11:18:47 +0900

    ..

---
+++ delivery_time_predict/lm_model.R
@@ -0,0 +1,21 @@
+# dataset 
+# 0. install packages 
+options(scipen=10)
+
+ipak <-function(pkg){
+  new.pkg<-pkg[!(pkg %in% installed.packages()[,"Package"])]
+  if(length(new.pkg))
+    install.packages(new.pkg,dependencies=TRUE)
+  sapply(pkg,require,character.only=TRUE)
+}
+
+pkg <- c("readr", "dplyr", "tidytext", "tidyverse", "lubridate", "reshape2", "psych", "gtsummary", "readxl", "MASS") # nolint
+ipak(pkg)
+
+##########################################################################################################################################################
+# data load
+data <- read_excel("/Users/yj.noh/Desktop/infra.xlsx") 
+head(data)
+
+# 배차소요시간
+model1 <- lm(배차소요시간 ~)
